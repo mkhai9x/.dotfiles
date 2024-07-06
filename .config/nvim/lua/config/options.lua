dd9b63316832c6a85d623b4c11660beb0e54d95c
Minh Tran <mkhai9x@gmail.com>
2024-07-07 01:16:48 +1000

    create first .dotfiles for using stow

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,5 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+--vim.opt.laststatus = 0 Since we have incline plugin already
